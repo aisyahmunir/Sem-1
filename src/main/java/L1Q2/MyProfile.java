69bbfb20a9c89e1af9f76d30cd40b2e4f1ae5ba7
aisyahmunir <75417443+aisyahmunir@users.noreply.github.com>
2021-09-01 21:56:02 +0800

    Add files via upload

---
+++ src/main/java/L1Q2/MyProfile.java
@@ -0,0 +1,13 @@
+package L1Q2;
+
+public class MyProfile {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Name : Aisyah binti Shaharol Munir");
+        
+        System.out.println("Matric no. : 17206198");
+        
+        System.out.println("Email : aisyahmunir2@gmail.com");
+    }
+}
